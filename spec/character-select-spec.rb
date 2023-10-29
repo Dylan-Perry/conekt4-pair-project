c532f98c49fa47322256b2272eb05da6d60ae93d
Stephen Nash <stephen.aa.nash@gmail.com>
2023-10-29 12:38:55 -0400

    Add experimental menu and character selection methods

--- spec/character-select-spec.rb
+++ spec/character-select-spec.rb
@@ -1,11 +1,16 @@
 RSpec.describe CharacterSelect do
 
   it 'exists' do
-     = CharacterSelect.new
+    default = CharacterSelect.new
 
-    expect().to be_an_instance_of(CharacterSelect)
+    expect(default).to be_an_instance_of(CharacterSelect)
   end
 
+  it 'displays a welcome message' do
+
+
+    expect(default.display_welcome_message).to eq("Connect four in a row to win!")
+  end
 
   
 end
