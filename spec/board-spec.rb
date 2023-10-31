a64c10955946772f95b122d72114cd82e2250da8
Dylan Perry <dylanbperry@gmail.com>
2023-10-30 22:12:24 -0700

    Fix missing matrix requirement and syntactical typos in exists and display_board tests

--- spec/board-spec.rb
+++ spec/board-spec.rb
@@ -1,23 +1,26 @@
 require './spec/spec-runner'
+require "matrix"
 
 RSpec.describe Board do
+
+  before :each do
+    conekt4_board = Board.new(6, 7)
+  end
 
   it 'exists' do
     conekt4_board = Board.new(6, 7)
 
     expect(conekt4_board).to be_a Board
     expect(conekt4_board.board_matrix).to be_a Matrix
-    expect(conekt4_board.column(0)).to eq(Vector["◌", "◌", "◌", "◌", "◌", "◌"])
-    expect(conekt4_board.row(0)).to eq(Vector["◌", "◌", "◌", "◌", "◌", "◌", "◌"])
+    expect(conekt4_board.board_matrix.column(0)).to eq(Vector["◌", "◌", "◌", "◌", "◌", "◌"])
+    expect(conekt4_board.board_matrix.row(0)).to eq(Vector["◌", "◌", "◌", "◌", "◌", "◌", "◌"])
   end
 
   describe "#display_board" do
-    before :each do
-      conekt4_board = Board.new(6, 7)
-    end
+    conekt4_board = Board.new(6, 7)
 
     it "displays an empty board, with column selection header row" do
-      expect{puts board.board_matrix}.to output("Matrix[[◌, ◌, ◌, ◌, ◌, ◌, ◌], [◌, ◌, ◌, ◌, ◌, ◌, ◌], [◌, ◌, ◌, ◌, ◌, ◌, ◌], [◌, ◌, ◌, ◌, ◌, ◌, ◌], [◌, ◌, ◌, ◌, ◌, ◌, ◌], [◌, ◌, ◌, ◌, ◌, ◌, ◌]]").to_stdout
+      expect{puts conekt4_board.board_matrix}.to output("Matrix[[◌, ◌, ◌, ◌, ◌, ◌, ◌], [◌, ◌, ◌, ◌, ◌, ◌, ◌], [◌, ◌, ◌, ◌, ◌, ◌, ◌], [◌, ◌, ◌, ◌, ◌, ◌, ◌], [◌, ◌, ◌, ◌, ◌, ◌, ◌], [◌, ◌, ◌, ◌, ◌, ◌, ◌]]\n").to_stdout
     end
   end
 
