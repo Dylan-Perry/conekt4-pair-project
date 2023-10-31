6f87dd06be198f159e4fdf47d489f2fb03e46578
Dylan Perry <dylanbperry@gmail.com>
2023-10-30 22:14:45 -0700

    Remove commented out notes that were transferred to readme

--- spec/board-spec.rb
+++ spec/board-spec.rb
@@ -23,14 +23,4 @@ RSpec.describe Board do
       expect{puts conekt4_board.board_matrix}.to output("Matrix[[◌, ◌, ◌, ◌, ◌, ◌, ◌], [◌, ◌, ◌, ◌, ◌, ◌, ◌], [◌, ◌, ◌, ◌, ◌, ◌, ◌], [◌, ◌, ◌, ◌, ◌, ◌, ◌], [◌, ◌, ◌, ◌, ◌, ◌, ◌], [◌, ◌, ◌, ◌, ◌, ◌, ◌]]\n").to_stdout
     end
   end
-
-  # Would RandomBoard this need a seperate class? Or can you randomize the initialize method chosen??
-  # it 'exists' do #would work as character-select? drop-down menu?
-  #   random_board = Board.new(random_columns, random_rows)
-
-  #   expect(random_game).to be_an_instance_of(Board) #(RandomBoard?)
-  # end
-
 end
-
-#Additional 'quote notes' for main branch upon pull and agreement
