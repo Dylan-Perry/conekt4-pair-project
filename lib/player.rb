ad0b99ad3971c12ccbf6387da540c8e1c1e162ce
Dylan Perry <dylanbperry@gmail.com>
2023-11-01 13:58:50 -0700

    Refactor player class to differentiate between human and computer players, and to set human players as computer

--- lib/player.rb
+++ lib/player.rb
@@ -1,28 +1,58 @@
 class Player
 
-  attr_reader :name, :piece
+  # (piece type handled by board?)
 
+  attr_reader :human
+
+  def initialize
+    @human = true
+  end
+
+  def make_computer
+    @human = false
+  end
+
+  def human?
+    @human
+  end
+end
   
+    # def take_turn
+    #   if @human
+    #     player_input(column_selection)
+    #   else
+    #     play_random_piece
+    #   end
+    # end
+  
+    # def player_input(column_selection)
+    #   puts "Enter the column number to place your piece:"
+    #   column_selection = gets.chomp.to_i
+    #   if valid_move?(column_selection)
+    #     @play_piece(column_selection)
+    #   else
+    #     p "Error: Invalid column selection"
+    #   end
+    # end
+    
+    # #I think it allows us to loop random numbers
+    # def play_random_piece(column_selection)
+    #   column_selection = rand(1..6)
+    #   column_selection = gets.chomp.to_i
+    #     if valid_move?(column_selection)
+    #       @play_piece(column_selection)
+    #     else
+    #       false #loops back???? or play_random_piece?
+    #     end
+    # end
 
-  def initialize(name, piece)
-    @players = []
-    @default_human = "\u{1F640}" #scared-cat
-    @secondary_human = "\u{1F3C3}" #jack-o-lantern
-    @default_cpu = "\u{1F3C3}" #jack-o-lantern / #random
-    @jigsaw_cpu = "\u{2699}" #gear / #always block
-    @jason_cpu = "\u{1F52A}" #kitchen-knife" / #always win
-
-  end
-
-
-end
-
-class CPU
-  attr_reader :name, :piece
-
-  def initialize(name, piece)
-    @name = name
-    @piece = piece
-  end
-
-end
+    # def valid_move?
+    #   if column_selection < 1 || column_selection > @columns
+    #     false
+    #   elsif @board_matrix.column(column_selection - 1)[0] != "\u{25cc}"
+    #     false
+    #   else
+    #     true # true here because it would have played a piece next
+    #   end
+    
+    # end
