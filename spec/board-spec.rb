7f603f79051ffc379aacb1858d489f14c8ba16af
Stephen Nash <stephen.aa.nash@gmail.com>
2023-11-01 17:52:34 -0400

    test:Add rspec tests for each class

--- spec/board-spec.rb
+++ spec/board-spec.rb
@@ -4,42 +4,43 @@ require "matrix"
 RSpec.describe Board do
 
   it 'exists' do
-    conekt4_board = Board.new(6, 7)
+    board = Board.new(6, 7)
 
-    expect(conekt4_board).to be_a Board
-    expect(conekt4_board.board_matrix).to be_a Matrix
-    expect(conekt4_board.board_matrix.column(0)).to eq(Vector["◌", "◌", "◌", "◌", "◌", "◌"])
-    expect(conekt4_board.board_matrix.row(0)).to eq(Vector["◌", "◌", "◌", "◌", "◌", "◌", "◌"])
+    expect(board).to be_a Board
+    expect(board.board_matrix).to be_a Matrix
+    expect(board.board_matrix.column(0)).to eq(Vector["◌", "◌", "◌", "◌", "◌", "◌"])
+    expect(board.board_matrix.row(0)).to eq(Vector["◌", "◌", "◌", "◌", "◌", "◌", "◌"])
   end
 
   describe "#display_board" do
-    conekt4_board = Board.new(6, 7)
+    board = Board.new(6, 7)
 
     it "displays an empty board, with column selection header row" do
-      expect{puts conekt4_board.board_matrix}.to output("Matrix[[◌, ◌, ◌, ◌, ◌, ◌, ◌], [◌, ◌, ◌, ◌, ◌, ◌, ◌], [◌, ◌, ◌, ◌, ◌, ◌, ◌], [◌, ◌, ◌, ◌, ◌, ◌, ◌], [◌, ◌, ◌, ◌, ◌, ◌, ◌], [◌, ◌, ◌, ◌, ◌, ◌, ◌]]\n").to_stdout
+      expect{puts board.board_matrix}.to output("Matrix[[◌, ◌, ◌, ◌, ◌, ◌, ◌], [◌, ◌, ◌, ◌, ◌, ◌, ◌], [◌, ◌, ◌, ◌, ◌, ◌, ◌], [◌, ◌, ◌, ◌, ◌, ◌, ◌], [◌, ◌, ◌, ◌, ◌, ◌, ◌], [◌, ◌, ◌, ◌, ◌, ◌, ◌]]\n").to_stdout
     end
   end
 
   describe "#play_piece" do
-    conekt4_board = Board.new(6, 7)
+    board = Board.new(6, 7)
 
     it "stacks pieces in a column until column is full, then prints an error message when selecting full column" do
-      expect(conekt4_board.board_matrix.column(2)).to eq(Vector["◌", "◌", "◌", "◌", "◌", "◌"])
+      expect(board.board_matrix.column(2)).to eq(Vector["◌", "◌", "◌", "◌", "◌", "◌"])
       
-      conekt4_board.play_piece(3)
-      conekt4_board.play_piece(3)
-      conekt4_board.play_piece(3)
-      conekt4_board.play_piece(3)
-      conekt4_board.play_piece(3)
-      conekt4_board.play_piece(3)
+      board.play_piece(3,true)
+      board.play_piece(3,true)
+      board.play_piece(3,true)
+      board.play_piece(3,true)
+      board.play_piece(3,true)
+      board.play_piece(3,true)
 
-      expect(conekt4_board.board_matrix.column(2)).to eq(Vector["X", "X", "X", "X", "X", "X"])
-      expect(conekt4_board.play_piece(3)).to eq("Error: Column is already full.")
+      expect(board.board_matrix.column(2)).to eq(Vector["X", "X", "X", "X", "X", "X"])
+      expect(board.play_piece(3,true)).to eq("Error: Column is already full.")
     end
+    
 
     it "errors out when playing a piece in a non-existent column" do
-      expect(conekt4_board.play_piece(0)).to eq("Error: Selected column does not exist.")
-      expect(conekt4_board.play_piece(8)).to eq("Error: Selected column does not exist.")
+      expect(board.play_piece(0,true)).to eq("Error: Selected column does not exist.")
+      expect(board.play_piece(8,true)).to eq("Error: Selected column does not exist.")
     end
   end
 end
--- spec/game-spec.rb
+++ spec/game-spec.rb
@@ -6,24 +6,7 @@ RSpec.describe Game do
     game = Game.new
     expect(game).to be_an_instance_of(Game)
   end
-
-  # it 'displays welcome message' do
-  #   expect(game.display_welcome_message).to eq("Welcom3 to Conekt4!").and eq("Connect four pieces in a row to win!")
-  # end
-    
-  # it 'displays player selection' do
-  #   expect(@new_game.display_player_selection).to eq("Choose wisely...").and eq("1 = Human v CPU").and eq("2 = Run away scaredy cat!")
-  # end
-
-  # it 'displays the game board' do
-  #   expect(@new_game.start_game).to be_an_instance_of(Board)
-  # end
   
-  # it 'uses inputs for player selection' do
-  #   expect(input == 1).to be_an_instance_of(start_game)
-  #   expect(input == 2).to be_an_instance_of(abort)
-  #   expect(input != 1 || input != 2).to be_an_instance_of(display_player_selection)
-  # end
 
 
 end
