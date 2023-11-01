d8904c039d46cfee26fd8786ea6d55099796002e
Stephen Nash <stephen.aa.nash@gmail.com>
2023-11-01 14:41:03 -0400

    feat & refactor: Add player class & refactor play_piece

--- lib/player.rb
+++ lib/player.rb
@@ -1,28 +1,38 @@
 class Player
 
-  attr_reader :name, :piece
+# (piece type handled by board?)
 
+  def initialize(human = true)
+    @human = human
+  end
+
+  def take_turn
+    if @human
+      player_input(column_selection)
+    else
+      play_random_piece
+    end
+  end
+
+  def player_input(column_selection)
+    puts "Enter the column number to place your piece:"
+    column_selection = gets.chomp.to_i
+    if valid_move?(column_selection)
+      @play_piece(column_selection)
+    else
+      p "Error: Invalid column selection"
+    end
+  end
   
-
-  def initialize(name, piece)
-    @players = []
-    @default_human = "\u{1F640}" #scared-cat
-    @secondary_human = "\u{1F3C3}" #jack-o-lantern
-    @default_cpu = "\u{1F3C3}" #jack-o-lantern / #random
-    @jigsaw_cpu = "\u{2699}" #gear / #always block
-    @jason_cpu = "\u{1F52A}" #kitchen-knife" / #always win
-
+  #I think it allows us to loop random numbers
+  def play_random_piece(column_selection)
+    column_selection = rand(1..6)
+    column_selection = gets.chomp.to_i
+      if valid_move?(column_selection)
+        @play_piece(column_selection)
+      else
+        false #loops back???? or play_random_piece?
+      end
   end
-
-
-end
-
-class CPU
-  attr_reader :name, :piece
-
-  def initialize(name, piece)
-    @name = name
-    @piece = piece
+  
   end
-
-end
